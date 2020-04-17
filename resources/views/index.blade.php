7a001839363e0c3a4d5d32328dc2e49c4c4e32eb
Adedeji Rotibi <dayjeerow7@gmail.com>
2020-04-17 17:11:31 +0100

    Fix issues

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -60,7 +60,7 @@
                     <h2 class="h1 font-weight-semi-bold mb-5" style="line-height:1.3">
                         Made for creators.<br>Designed for you.
                     </h2>
-                    <h4 class="h5 font-weight-light">
+                    <h4 class="h5 font-weight-light text-muted">
                         Our products and templates are streamlined for easy customization. Our focus is on the latest
                         technology trends, modern design and great user experience.
                     </h4>
@@ -108,7 +108,8 @@
                                         alt="SVG" data-parent="#SVGicons">
                                 </figure>
                                 <h3 class="h6 font-weight-normal">Get Support</h3>
-                                <p class="small px-3">Get fast, professional support through our online community of creators.</p>
+                                <p class="small px-3">Get fast, professional support through our online community of
+                                    creators.</p>
                             </div>
                         </div>
                     </div>
@@ -117,4 +118,60 @@
         </div>
     </div>
 </main>
+
+<footer style="background-color:black">
+    <div class="container space-top-1 space-bottom-2">
+        <div class="row text-white">
+            <div class="col-lg-3 mb-7 mb-lg-0">
+                <div class="d-flex align-self-start flex-column h-100">
+                    <a class="d-flex align-items-center mb-3" href="/" aria-label="Pag.es">
+                        <span class="brand brand-primary font-weight-normal text-white">Pag.es</span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-4 mb-md-0 text-right">
+                <h4 class="h6 font-weight-semi-bold">Products</h4>
+
+                <ul
+                    class="list-group list-group-transparent list-group-white list-group-flush list-group-borderless mb-0">
+                    <li><a class="list-group-item list-group-item-action" href="#">Pages</a></li>
+                    <li><a class="list-group-item list-group-item-action" href="#">Signatures</a></li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-4 mb-md-0 text-right">
+                <h4 class="h6 font-weight-semi-bold">Company</h4>
+
+                <ul
+                    class="list-group list-group-transparent list-group-white list-group-flush list-group-borderless mb-0">
+                    <li><a class="list-group-item list-group-item-action" href="#">About</a></li>
+                    <li><a class="list-group-item list-group-item-action" href="#">Press</a></li>
+                </ul>
+            </div>
+
+            <div class="col-sm-4 col-md-3 col-lg-2 mb-4 mb-md-0 text-right">
+                <h4 class="h6 font-weight-semi-bold">Legal</h4>
+
+                <ul
+                    class="list-group list-group-transparent list-group-white list-group-flush list-group-borderless mb-0">
+                    <li><a class="list-group-item list-group-item-action" href="#">Privacy</a></li>
+                    <li><a class="list-group-item list-group-item-action" href="#">Terms</a></li>
+                    <li><a class="list-group-item list-group-item-action" href="#">GDPR</a></li>
+                </ul>
+            </div>
+
+            <div class="col-sm-4 col-md-3 col-lg-2 mb-4 mb-md-0 text-right">
+                <h4 class="h6 font-weight-semi-bold">Connect</h4>
+
+                <ul
+                    class="list-group list-group-transparent list-group-white list-group-flush list-group-borderless mb-0">
+                    <li><a class="list-group-item list-group-item-action" href="#">Instagram</a></li>
+                    <li><a class="list-group-item list-group-item-action" href="#">Twitter</a></li>
+                    <li><a class="list-group-item list-group-item-action" href="#">Community</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
